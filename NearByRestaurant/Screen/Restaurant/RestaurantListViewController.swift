51c25a32b773906de6b2eb58f53b307e87007b55
Nirbhay Singh <nirbhay2992@gmail.com>
2022-04-05 01:49:30 +0530

    RestaurantListViewController file added

---
+++ NearByRestaurant/Screen/Restaurant/RestaurantListViewController.swift
@@ -0,0 +1,78 @@
+//
+//  RestaurantListViewController.swift
+//  NearByRestaurant
+//
+//  Created by Nirbhay Singh on 04/04/22.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController {
+
+    // MARK: IBOutlet
+    @IBOutlet private weak var restaurantTableView: UITableView!
+    
+    // MARK: Properties
+    private var viewModel: RestaurantListViewModel?
+    
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initiateViewModel()
+        
+        restaurantTableView.rowHeight = UITableView.automaticDimension
+        restaurantTableView.estimatedRowHeight = 100
+        restaurantTableView.separatorStyle = .none
+        restaurantTableView.separatorColor = .clear
+        restaurantTableView.register(RestaurantTableViewCell.self, forCellReuseIdentifier: RestaurantTableViewCell.identifier)
+
+    }
+
+    
+    //MARK: ViewModel
+    private func initiateViewModel() {
+        viewModel = RestaurantListViewModel(service: Service())
+        viewModel?.reloadTableView = { [weak self] in
+            DispatchQueue.main.async { [weak self] in
+                self?.restaurantTableView.reloadData()
+            }
+        }
+        viewModel?.getRestaurantList()
+    }
+}
+
+
+extension RestaurantListViewController : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: .zero)
+        return view
+    }
+}
+
+extension RestaurantListViewController : UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        viewModel?.restaurents?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell: RestaurantTableViewCell = tableView.dequeueReusableCell(withIdentifier: RestaurantTableViewCell.identifier) as? RestaurantTableViewCell {
+            
+            if let cellViewModel = viewModel?.getCellViewModel(indexPath: indexPath) {
+                cell.configure(viewModel: cellViewModel)
+            }
+            
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
